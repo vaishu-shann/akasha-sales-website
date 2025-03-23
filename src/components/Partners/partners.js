a75f8675cead4de6caa886d3cc9c2427dbf5a8e0
vaishu-shann <vaishnaviramashanmugam@gmail.com>
2025-03-23 11:41:43 +0530

    Mobile responsiveness updated

--- src/components/Partners/partners.js
+++ src/components/Partners/partners.js
@@ -5,7 +5,7 @@ import Partner2 from "../../assets/partner2.png"
 import Partner3 from "../../assets/partner3.png"
 import Partner4 from "../../assets/partner4.png"
 import Partner5 from "../../assets/partner5.png"
-
+import Marquee from "react-fast-marquee";
 
 const Partners = () => {
   return (
@@ -19,6 +19,18 @@ const Partners = () => {
         <img src={Partner3} className='partner-img' alt=''/>
         <img src={Partner4} className='partner-img' alt=''/>
         <img src={Partner5} className='partner-img' alt=''/>
+      </div>
+
+      <div className='marquee'>
+      <Marquee speed={50} direction="right">
+      <img src={Partner1} className='partner-img' alt=''/>
+        <img src={Partner2} className='partner-img' alt=''/>
+        <img src={Partner3} className='partner-img' alt=''/>
+        </Marquee>
+        <Marquee speed={50} direction='left'>
+        <img src={Partner4} className='partner-img' alt=''/>
+        <img src={Partner5} className='partner-img' alt=''/>
+        </Marquee>
       </div>
     </div>
   )
